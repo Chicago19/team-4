4f9d80a605c6803aa2351196dd028b7cc30a7237
Malik <arachich@msu.edu>
2019-09-20 22:20:32 -0500

    Added a functioning tableView with fake data

---
+++ CodeForGood2019/ResourcesTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ResourcesTableViewCell.swift
+//  CodeForGood2019
+//
+//  Created by Malik Arachiche on 9/20/19.
+//  Copyright © 2019 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+
+class ResourcesTableViewCell: UITableViewCell {
+    
+    
+    
+}
