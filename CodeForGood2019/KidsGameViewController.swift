707c45379978925effa0ff23c7db0d41e359b996
Meghan <mrj9235@rit.edu>
2019-09-21 08:13:59 -0500

    adding open app functionality to KidsGames

--- CodeForGood2019/KidsGameViewController.swift
+++ CodeForGood2019/KidsGameViewController.swift
@@ -12,10 +12,43 @@ import UIKit
 class KidGamesViewController: UIViewController {
     
     @IBOutlet weak var controllerView: UICollectionView!
+    @IBOutlet weak var flowButton: UIButton!
+    @IBOutlet weak var sonicButton: UIButton!
+    @IBOutlet weak var flowImage: UIImageView!
+    @IBOutlet weak var sonicImage: UIImageView!
+    
+    var kidsArray: [Game] = [Game(name: "Flow", image: UIImage(named: "flow3")!),
+                             
+                             Game(name: "bubble", image: UIImage(named: "bubbleIconTransparency")!),
+                             
+                             Game(name: "bike", image: UIImage(named: "bikeIconTransparent")!),
+                             
+                             Game(name: "helixJump", image: UIImage(named: "helixIconTransparent")!),
+                             
+                             Game(name: "nails", image: UIImage(named: "nailSalonIconTransparent")!),
+                             
+                             Game(name: "sonic", image: UIImage(named: "sonicIconTransparent")!),
+                             
+                             Game(name: "subwaySurfers", image: UIImage(named: "subwayIconTransparent")!),
+                             
+                             Game(name: "ticTacToe", image: UIImage(named: "ticTacToeIconTransparent")!)
+    
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        controllerView.layer.borderColor = UIColor.clear.cgColor
+        
     }
+    
+    
+    @IBAction func flowButtonPressed(_ sender: UIButton) {
+        flowImage.isHidden = false;
+    }
+    
+    @IBAction func sonicButtonPressed(_ sender: UIButton){
+        sonicImage.isHidden = false;
+    }
+    
 }
+
