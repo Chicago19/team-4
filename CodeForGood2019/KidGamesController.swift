f920d89a2e607c6576ea4d55a6ed501223825155
Meghan <mrj9235@rit.edu>
2019-09-20 21:16:48 -0500

    added story board and swift file

---
+++ CodeForGood2019/KidGamesController.swift
@@ -0,0 +1,9 @@
+//
+//  KidGamesController.swift
+//  CodeForGood2019
+//
+//  Created by Meghan Johnson on 9/20/19.
+//  Copyright © 2019 Malik Arachiche. All rights reserved.
+//
+
+import Foundation
