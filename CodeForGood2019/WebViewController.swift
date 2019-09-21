c495af54b8912dd7187d24f3d96cce423977990e
Tortera1999 <nikhilnarendra1999@gmail.com>
2019-09-20 23:58:28 -0400

    Added the web view controller

---
+++ CodeForGood2019/WebViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WebViewController.swift
+//  CodeForGood2019
+//
+//  Created by Nikhil Iyer on 9/20/19.
+//  Copyright © 2019 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKUIDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
